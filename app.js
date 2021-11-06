33729f5ef9c381a50399849e0ddbce6092cdfdd9
shalini jhajharia <shalinijhajharia185@gmail.com>
2021-11-06 14:58:36 +0530

    updated

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 //Enter your code here..
+function getcontacts()
+{
+fetch('https://jsonplaceholder.typicode.com/users').then(res=>{
+    return(res.json());
+})
+.then(res2 =>{
+    console.log(res2);
+    var player='<h2>Users Lists</h2>';
+                    res2.forEach(function(user) {
+                     player+=
+                    `<div class="player">
+                      <div class="strength">Name : ${user.name}</div>
+                      <div>Email   : ${user.email}</div>
+                      <div>Phone   : ${user.phone}</div>
+                      <div>Website : ${user.website}</div>
+                      <div>Company : ${user.company.name}</div>
+                      <div>City    : ${user.address.city}</div>
+                      <div>Zipcode : ${user.address.zipcode}</div>
+                     </div>
+                    `
+                    })
+                    document.getElementById('message').innerHTML=player;
+})
+.catch(err=>{
+    console.log(err);
+})
+}
